1108224df17888eb6bf3d6853716f4ac8584f0f1
Cblpka <vehluncho@gmail.com>
2017-06-12 19:12:30 +0300

    Initial Commit

---
+++ test_work_app/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test_work_app
+//
+//  Created by Admin on 12.06.17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test_work_app/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test_work_app
+//
+//  Created by Admin on 12.06.17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
